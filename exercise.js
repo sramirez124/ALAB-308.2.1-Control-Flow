02eb05828ba2df4156e2459000d07ae73ca54b65
sramirez124 <forwardemails124@gmail.com>
2024-09-12 12:11:03 -0500

    basic logic added
    not checked yet if it works

--- exercise.js
+++ exercise.js
@@ -4,3 +4,18 @@ const area = PI * radius * radius;
 const minSpace = 0.8;
 const startingPlants = 20;
 const growthRate = 2;
+const maxPlantCompacity = (area / minSpace);
+
+// First Week
+
+const firstWeekPlantCount = startingPlants * growthRate;
+const firstWeekSpace = firstWeekPlantCount * minSpace;
+const firstWeekSpaceLeft = area - firstWeekSpace;
+
+//area check
+// console.log('Area is ' + area);
+
+if ((firstWeekPlantCount / maxPlantCompacity) < 0.8){
+    console.log("First wee plant count: " + firstWeekPlantCount)
+    console.log("The plant needs ot be pruned");
+}
