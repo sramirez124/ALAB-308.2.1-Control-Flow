5bf994be33479fb047fb426ca0e5dcb54c8d8173
sramirez124 <forwardemails124@gmail.com>
2024-09-12 12:39:25 -0500

    first part of lab done

--- exercise.js
+++ exercise.js
@@ -10,16 +10,61 @@ const maxPlantCompacity = (area / minSpace);
 const firstWeekPlantCount = startingPlants * growthRate;
 const firstWeekSpace = firstWeekPlantCount * minSpace;
 const firstWeekSpaceLeft = area - firstWeekSpace;
+const firstWeekSpaceTaken = firstWeekSpace / area;
 
-
+console.log("==============First Week==============")
 if ((firstWeekPlantCount / maxPlantCompacity) < 0.8){
     console.log("Week 1: The plants need to be pruned");
+    console.log("Space taken: " + firstWeekSpaceTaken * 100 + "%");
 } else if ((firstWeekPlantCount * minSpace) / area >= 0.5) {
     console.log("Week 1: The plants does not need to be pruned")
+    console.log("Space taken: " + firstWeekSpaceTaken * 100 + "%");
 } else {
     console.log("Week 1: You can plant more...plants")
+    console.log("Space taken: " + firstWeekSpaceTaken * 100 + "%");
 }
 
 // Second Week
 const secondWeekPlantCount = firstWeekPlantCount * growthRate;
 const secondWeekSpace = secondWeekPlantCount * minSpace;
+const secondWeekSpaceTaken = secondWeekSpace / area;
+
+console.log("==============Second Week==============")
+switch (secondWeekSpace / area){
+    case secondWeekSpace / area > 0.8:
+        console.log("The plants need to be pruned");
+        console.log("Space taken: " + secondWeekSpaceTaken * 100 + "%");
+        break;
+    
+    case secondWeekSpace / area >= 0.5:
+        console.log("The plants do not need to be pruned");
+        console.log("Space taken: " + secondWeekSpaceTaken * 100 + "%");
+        break;
+    
+    default:
+        console.log("You can plant more");
+        console.log("Space taken: " + secondWeekSpaceTaken * 100 + "%");
+}
+
+// Third Week
+
+const thirdWeekPlantCount = firstWeekPlantCount * growthRate;
+const thirdWeekSpace = thirdWeekPlantCount * minSpace;
+const thirdWeekSpaceTaken = thirdWeekSpace / area;
+
+console.log("==============Third Week==============")
+switch (thirdWeekSpace / area){
+    case thirdWeekSpace / area > 0.8:
+        console.log("The plants need to be pruned");
+        console.log("Space taken: " + thirdWeekSpaceTaken * 100 + "%");
+        break;
+    
+    case thirdWeekSpace / area >= 0.5:
+        console.log("The plants do not need to be pruned");
+        console.log("Space taken: " + thirdWeekSpaceTaken * 100 + "%");
+        break;
+    
+    default:
+        console.log("You can plant more");
+        console.log("Space taken: " + thirdWeekSpaceTaken * 100 + "%");
+}
